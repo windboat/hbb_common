41fa7d7f8bff25524be7f12d081dd350d0f0bc62
thomas.zhou <windboat2023@gmail.com>
2025-05-18 02:09:12 +0800

    add self network config

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.120.75.159"];
+pub const RS_PUB_KEY: &str = "lfDCp4v2HwI0evPbHrqoqN8x2j8uKgorpInXsbpH+kc=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
